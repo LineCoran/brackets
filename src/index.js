5d84c9b792280fadc66833e417250297a1bf515d
Alexey Kuzmichev <104069255+LineCoran@users.noreply.github.com>
2022-06-25 20:26:07 +0300

    feat:
    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = (str, bracketsConfig) => {
+  let brackets = Object.fromEntries(bracketsConfig);
+  for (var i = 0; i<str.length; i++){
+    if (!thereIsPair()) return false;
+  }
+  function thereIsPair() {
+    let mirror = brackets[str[i]];
+    for (i++; i<str.length; i++){
+      if (str[i] === mirror) return true;
+      if (!thereIsPair()) return false;
+      }
+    }
+    return true;
 }
